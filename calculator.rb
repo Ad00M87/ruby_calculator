a0af9d24276876c53f8bf6c5a5323cc6f9e0263c
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-08-16 17:30:13 -0600

    clear and quit working

--- calculator.rb
+++ calculator.rb
@@ -9,13 +9,33 @@ require 'colorize'
 
 def start
   puts "Welcome to the Ruby Calculator".colorize(:yellow)
+  puts "If you would like to exit at anytime type quit".colorize(:red)
   puts "What is the first number?"
-  number_1 = gets.to_f
+  number_1 = gets.strip
+  if /(\d)|(\d+).(\d+)/.match(number_1)
+    num1f = number_1.to_f
+  elsif /quit/.match(number_1)
+    exit
+  else
+    puts "Invalid input. Please enter a number."
+    start
+  end
   puts "What is the operator?"
   operator = gets.strip
+  if /quit/.match(operator)
+    exit
+  end
   puts "What is the second number?"
-  number_2 = gets.to_f
-  calculator(number_1, number_2, operator)
+  number_2 = gets.strip
+  if /(\d+)|(\d+).(\d+)/.match(number_2)
+    num2f = number_2.to_f
+  elsif /quit/.match(number_2)
+    exit
+  else
+    puts "Invalid input. Please enter a number."
+    start
+  end
+  calculator(num1f, num2f, operator)
 end
 
 def calculator(left, right, operator)
@@ -48,10 +68,19 @@ def calculator(left, right, operator)
 end
 
 def second_pass(answer)
+  puts "Type clear to clear previous value."
   puts "What is the operator?"
   operator = gets.strip
+  if /quit/.match(operator)
+    exit
+  elsif /clear/.match(operator)
+    start
+  end
   puts "What is the second number?"
-  number_2 = gets.to_i
+  number_2 = gets.to_f
+  if /quit/.match(number_2)
+    exit
+  end
   calculator(answer, number_2, operator)
 end
 
